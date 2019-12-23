76195c29b1d156cf69219724fa27087676965fd7
Eric Yang <yangeric12@gmail.com>
2019-12-23 13:56:27 -0500

    Liftoff! - finished section 2

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// SELF-NOTE: test what happens when we remove the above line
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>Hello from React</div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './app/index.js',
+  mode: 'development',
+  output: {
+    path: __dirname,
+    filename: 'public/wpbundle.js'
+  },
+  devtool: 'source-maps',
+  // a source map maps the optimized compressed code to the original,
+  // making debugging simpler
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // best practice is to only use RegExp in test for file matching
+        // '\' escapes '.', turning it into a literal
+        // '$' denotes end of string
+        // matches all files with '.js' at the end
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
